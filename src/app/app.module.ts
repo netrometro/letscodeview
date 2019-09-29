79c11bf3bacf3e1c5ab9fa708baae8edffda7efd
netrometro <netrometro@yahoo.com.br>
2019-09-28 23:37:54 -0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { RouterModule } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -22,7 +24,10 @@ import { routes } from './app.routes';
     CardModule, 
     PasswordModule, 
     ButtonModule, 
-    RouterModule ],
+    RouterModule,
+    MenuModule,
+    MenubarModule
+ ],
   declarations: [ AppComponent, LoginPageComponent, MenuPageComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AppService]
--- src/app/menu-page/menu-page.component.ts
+++ src/app/menu-page/menu-page.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu-page',
   templateUrl: './menu-page.component.html',
   styleUrls: ['./menu-page.component.css']
 })
 export class MenuPageComponent implements OnInit {
+  
+  items: MenuItem[];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'File',
+                items: [{
+                        label: 'New', 
+                        icon: 'pi pi-fw pi-plus',
+                        items: [
+                            {label: 'Project'},
+                            {label: 'Other'},
+                        ]
+                    },
+                    {label: 'Open'},
+                    {label: 'Quit'}
+                ]
+            },
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                    {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+                ]
+            }
+        ];
+    }
 }
